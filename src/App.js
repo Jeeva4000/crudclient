74528d48e594593660543ab9874557fc080782d3
Jeeva4000 <vjeeva@gmail.com>
2024-01-23 20:50:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+
+import { Route, Routes } from "react-router-dom"
+import Navbaar from './components/Navbaar.js';
+import Home from './components/Home.js';
+import Register from './components/Register.js';
+import Edit from './components/Edit.js';
+import Details from './components/Details.js';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbaar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/edit/:id" element={<Edit />} />
+        <Route exact path="/view/:id" element={<Details />} />
+      </Routes>
+
+    </>
   );
 }
 
 export default App;
+
+// import React from 'react'
+// import './App.css';
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+// import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+// import Navbaar from './elements/Navbaar.js'
+// import Home from './elements/Home.js'
+// import Register from './elements/Register.js';
+// import { Route, Routes } from 'react-router-dom';
+
+
+// function App() {
+//   return (
+//     <>
+//       <Navbaar />
+//       <Routes>
+//         <Route exact path="/" element={<Home />} />
+//         <Route exact path="/register" element={<Register />} />
+//         {/* <Route exact path="/edit/:id" element={} />
+//         <Route exact path="/view/:id" element={} /> */}
+//       </Routes>
+
+//     </>
+//   )
+// }
+
+// export default App
